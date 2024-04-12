0ae6d9bdaf8b9f54cb662c5da2771c8941514ce6
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-13 02:06:34 +0300

    updates lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Joan",
+    streetAddress:"Roysambu",
+}
+
+function updateEmployeeWithKeyAndValue (employee, name,Melvin){
+    const newemployee = {...employee}
+    newemployee[name]= Melvin
+    return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, streetAddress,Kasarani){
+    employee[streetAddress] = Kasarani;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, streetAddress){
+    const newemployee ={...employee}
+    delete newemployee[streetAddress];
+    return newemployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee,name){
+    delete employee[name];
+    return employee;
+}
